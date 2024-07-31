4b9465a90a27005dd43be8e9076bdaf6bb27af4f
bbl <bbl@host118.MAIN.MFVSEGEI.RU>
2024-07-31 16:17:48 +0300

    HW #41 commit #1

---
+++ src/main/java/telran/employees/net/CompanyProxy.java
@@ -0,0 +1,84 @@
+package telran.employees.net;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import telran.employees.Company;
+import telran.employees.Employee;
+import telran.employees.Manager;
+import telran.net.Request;
+import telran.net.TcpClient;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import telran.employees.Company;
+import telran.employees.Employee;
+import telran.employees.Manager;
+import telran.net.Request;
+import telran.net.TcpClient;
+
+public class CompanyProxy implements Company {
+     TcpClient tcpClient;
+     
+	public CompanyProxy(TcpClient tcpClient) {
+		this.tcpClient = tcpClient;
+	}
+
+	@Override
+	public Iterator<Employee> iterator() {
+		throw new UnsupportedOperationException("Iterator is not supported fro TCP proxy");
+	}
+
+	@Override
+	public void addEmployee(Employee empl) {
+		tcpClient.sendAndReceive(new Request("addEmployee", empl.getJSON()));
+
+	}
+
+	@Override
+	public Employee getEmployee(long id) {
+		return getRemoveEmployee("getEmployee",id);
+	}
+
+	private Employee getRemoveEmployee(String requestType, long id) {
+		String emplJSON = tcpClient.sendAndReceive
+				(new Request(requestType, "" + id));
+		return (Employee) new Employee().setObject(emplJSON);
+	}
+
+	@Override
+	public Employee removeEmployee(long id) {
+		return getRemoveEmployee("removeEmployee", id);
+	}
+
+	@Override
+	public int getDepartmentBudget(String department) {
+		
+		return Integer.parseInt(tcpClient
+				.sendAndReceive(new Request("getDepartmentBudget", department)));
+	}
+
+	@Override
+	public String[] getDepartments() {
+		
+		return tcpClient.sendAndReceive(new Request("getDepartments", ""))
+				.split(";");
+	}
+
+	@Override
+	public Manager[] getManagersWithMostFactor() {
+		String managersJSON = tcpClient.sendAndReceive(new Request("getManagersWithMostFactor", ""));
+		
+		return Arrays.stream(managersJSON.split(";"))
+				.map(s -> (Manager)new Employee().setObject(s))
+				.toArray(Manager[]::new);
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
